eb09b20cc60a173ecb369d967ea3e266b49a314c
Ashish Amgain <amgainashish@gmail.com>
2025-02-16 17:13:46 +0545

    making changes

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Very detailed request logging
+
 app.use((req, res, next) => {
   console.log('\n--- Incoming Request ---');
   console.log('URL:', req.url);
