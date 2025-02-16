82332e21e15e03ab77562bd0090220b40e61e1b3
Ashish Amgain <amgainashish@gmail.com>
2025-02-16 16:50:02 +0545

    Added new files

--- Frontend/src/pages/register/Register.jsx
+++ Frontend/src/pages/register/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { registerUserApi } from '../../apis/Api';
 import { toast } from 'react-toastify';
-import '../../CSS/Register.css';
+import '../../CSS/Register.css'; //importing register css
 
 const Register = () => {
   const [firstName, setFirstName] = useState('');
