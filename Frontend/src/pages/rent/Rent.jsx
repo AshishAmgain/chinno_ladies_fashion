6ffc86dedfa5f5dce49e9ec4ee04bb240ab7f6e8
Ashish Amgain <amgainashish@gmail.com>
2025-02-16 16:51:57 +0545

    making changes

--- Frontend/src/pages/rent/Rent.jsx
+++ Frontend/src/pages/rent/Rent.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import axios from "axios";
-import "../CSS/Rent.css";
+import "../CSS/Rent.css"; //css of rent
 import { getSingleProduct } from "../apis/Api";
 
 const Rent = () => {
