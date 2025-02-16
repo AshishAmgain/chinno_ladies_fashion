fbfd6abda2577d3ef9ae16ec3ee110f067aa7252
Ashish Amgain <amgainashish@gmail.com>
2025-02-16 17:13:22 +0545

    making changes

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ const cartRoutes = require('./routes/cartRoutes');
 dotenv.config();
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
